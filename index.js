d5c24641d2c9b92f9d575935fc1def3ee289a38d
Philipp Krüger <philipp.krueger1@gmail.com>
2020-06-01 17:44:21 +0200

    :books: Document decition to get rid of custom elements

--- index.js
+++ index.js
@@ -16,6 +16,26 @@ function codeComponentByLanguage(language) {
   }
 }
 
+// TODO Remove web components
+/*
+  I'm pretty sure webcomponents aren't the most beneficial way to go here:
+  Benefits:
+    * No need to have a model like this:
+      { carusels : Dict String Carusel.Model
+      , interactiveJs : Dict String InteractiveJs.Model
+      , interactiveElm : Dict String InteractiveElm.Model
+      , ...
+      }
+      and no need to handle their Msgs and stuff.
+  
+  Drawbacks:
+    * Has a weird lifecycle: Custom element's inner text is rendered after they already get connected (sometimes?)
+    * Have to pass any information via serialization: Code has to be passed as string/json, instead of being parsed directly.
+    * Have to handle custom code in my renderer anyway, since I want to syntax-check it. (and maybe even type-check).
+      This means I actually check my code twice (inside and outside the custom element...)
+    * Custom code and it's interactive renderings don't get SSRed. Kinda sucks. Especially normal syntax highlighting!
+      This way it's possible someone who didn't enable javascript gets some VERY wierd looking code examples (as I don't usually test that).
+*/
 customElements.define('custom-code',
   class extends HTMLElement {
     constructor() {
