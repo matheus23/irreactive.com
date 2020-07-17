ea21e15fd5c0ac36d636a65802e730c09f5e474b
Philipp Krüger <philipp.krueger1@gmail.com>
2020-07-17 14:25:08 +0200

    :sparkles: Improve submission created function

--- serverless-functions/submission-created.js
+++ serverless-functions/submission-created.js
@@ -1,25 +1,43 @@
 require('dotenv').config()
 const fetch = require('node-fetch')
 const { EMAIL_TOKEN } = process.env
+
 exports.handler = async (event, context, callback) => {
     const formName = event.queryStringParameters['form-name'];
     const email = event.queryStringParameters.email;
+
     if (formName !== 'email-subscription') {
-        return { statusCode: 404, body: `Unknown form: ${formName}` };
+        return {
+            statusCode: 404,
+            body: `Unknown form: ${formName}`
+        };
     }
+    if (EMAIL_TOKEN == null) {
+        return {
+            statusCode: 500,
+            body: `Cannot submit to buttondown: Missing EMAIL_TOKEN`
+        };
+    }
+
     console.log(`Recieved a submission: ${email}`)
-    console.log(`Is there an email token? ${EMAIL_TOKEN != null}`)
-    return fetch('https://api.buttondown.email/v1/subscribers', {
-        method: 'POST',
-        headers: {
-            Authorization: `Token ${EMAIL_TOKEN}`,
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email }),
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(`Submitted to Buttondown:\n ${JSON.stringify(data)}`)
-        })
-        .catch(error => ({ statusCode: 422, body: String(error) }))
+    try {
+        const response = await fetch('https://api.buttondown.email/v1/subscribers', {
+            method: 'POST',
+            headers: {
+                Authorization: `Token ${EMAIL_TOKEN}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email }),
+        });
+        console.log(`Submitted to Buttondown:\n ${JSON.stringify(response.json())}`);
+        return {
+            statusCode: 200,
+            body: `Successfully submitted ${email} to buttondown`
+        };
+    } catch (error) {
+        return {
+            statusCode: 422,
+            body: String(error)
+        };
+    }
 }
